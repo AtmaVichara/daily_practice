b3601466ba6e49b142829427d2ffb4e7ceb3cf55
Joe Jobes <jmrjobes@gmail.com>
2018-06-16 08:17:43 -0600

    Add printing, user input, and numbers practice

---
+++ java_practice/printing_practice/src/numbers/SecondsCalculator.java
@@ -0,0 +1,25 @@
+package numbers;
+
+import java.util.Scanner;
+
+public class SecondsCalculator {
+	public static void main(String[] args) {
+		int days = userInputDays();
+		int seconds = calculateSeconds(days);
+		System.out.println("There are " + seconds + " seconds in " + days + " days!");
+	}
+
+	public static int calculateSeconds(int days) {
+		int hours = days * 24;
+		int minutes = hours * 60;
+		int seconds = minutes * 60;
+		return seconds;
+	}
+
+	public static int userInputDays() {
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Please input days: ");
+		int days = reader.nextInt();
+		return days;
+	}
+}
