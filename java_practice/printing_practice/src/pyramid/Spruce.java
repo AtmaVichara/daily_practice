600b3db2429ba3a7c93b0cd29c905403fc22246a
Joe Jobes <jmrjobes@gmail.com>
2018-06-16 08:17:16 -0600

    Update logic by deleting second counter

--- java_practice/printing_practice/src/pyramid/Spruce.java
+++ java_practice/printing_practice/src/pyramid/Spruce.java
@@ -6,17 +6,15 @@ import java.util.stream.IntStream;
 public class Spruce {
 	public static void main(String[] args) {
 		int d = 8;
-		int ii = 1;
 		for (int i = 0; i < 9; i++) {
 			String whiteSpace = IntStream.range(0, d).mapToObj(x -> " ").collect(Collectors.joining(""));
 			if (i == 0) {
 				System.out.println(whiteSpace + "*" + whiteSpace);
 			} else {
-				String bangs = IntStream.range(0, (i + ii)).mapToObj(x -> "*").collect(Collectors.joining(""));
+				String bangs = IntStream.range(0, (i + (i + 1))).mapToObj(x -> "*").collect(Collectors.joining(""));
 				System.out.println(whiteSpace + bangs + whiteSpace);
 			}
 			d--;
-			ii++;
 		}
 	}
 }
