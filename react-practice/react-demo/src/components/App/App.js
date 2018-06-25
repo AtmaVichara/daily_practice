5699d6619646e5255979971e3f7ff2d563349fe9
Joe Jobes <jmrjobes@gmail.com>
2018-06-25 07:31:10 -0600

    Add to app component

--- react-practice/react-demo/src/components/App/App.js
+++ react-practice/react-demo/src/components/App/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import { Welcome } from "../Welcome/Welcome";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route
+            exact path="/"
+            component={Welcome}
+          />
+          <Route
+            exact path="/about"
+            component={Welcome}
+          />
+        </Switch>
       </div>
     );
   }
