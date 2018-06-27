be7e5203b8108c8f6c5cd8c70b266588d8cfd080
Joe Jobes <jmrjobes@gmail.com>
2018-06-27 07:18:00 -0600

    Add findMax(), replaceNode(), and Delete() to node

--- go_practice/binary_tree/node.go
+++ go_practice/binary_tree/node.go
@@ -51,3 +51,59 @@ func (n *Node) Find(s string) string {
       return n.Left.Find(s)
   }
 }
+
+func (n *Node) findMax(parent *Node) (*Node, *Node) {
+  if n == nil {
+    return nil, parent
+  }
+  if n.Right == nil {
+    return n, parent
+  }
+  return n.Right.findMax(n)
+}
+
+func (n *Node) replaceNode(parent, replacement *Node) error {
+  if n == nil {
+    return errors.New("replaceNode() cannot work on node nil!")
+  }
+
+  if n == parent.Left {
+    parent.Left = replacement
+    return nil
+  }
+  parent.Right = replacement
+  return nil
+}
+
+func (n *Node) Delete(s string, parent *Node) string {
+  if n == nil {
+    return "Node is nil"
+  }
+
+  switch {
+    case s > n.Value:
+      return n.Right.Delete(s, n)
+    case s < n.Value:
+      return n.Left.Delete(s, n)
+    default:
+      if n.Right == nil && n.Left == nil {
+        n.replaceNode(parent, nil)
+        return "Deleted " + s
+      }
+      if n.Left == nil {
+        n.replaceNode(parent, n.Right)
+        return "Deleted " + s
+      }
+      if n.Right == nil {
+        n.replaceNode(parent, n.Left)
+        return "Deleted " + s
+      }
+      replacement, replParent := n.Left.findMax(parent)
+
+      n.Value = replacement.Value
+      n.Data = replacement.Data
+
+      return replacement.Delete(replacement.Value, replParent)
+  }
+
+}
